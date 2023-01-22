f9bbd0ed6896ffd7c561a205a71392e0baf39566
Akshit Agrawal <71216807+justAkshitAgrawal@users.noreply.github.com>
2023-01-22 20:33:28 +0530

    Contact

--- src/components/EightRow.jsx
+++ src/components/EightRow.jsx
@@ -1,16 +1,68 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function EightRow() {
+  const navigate = useNavigate();
+
   return (
     <>
       <h1>X</h1>
-      <h1 className=" info">C</h1>
-      <h1 className=" info">O</h1>
-      <h1 className=" info">N</h1>
-      <h1 className=" info">T</h1>
-      <h1 className=" info">A</h1>
-      <h1 className=" info">C</h1>
-      <h1 className=" info">T</h1>
+      <h1
+        className=" info"
+        onClick={() => {
+          navigate("/contactme");
+        }}
+      >
+        C
+      </h1>
+      <h1
+        className=" info"
+        onClick={() => {
+          navigate("/contactme");
+        }}
+      >
+        O
+      </h1>
+      <h1
+        className=" info"
+        onClick={() => {
+          navigate("/contactme");
+        }}
+      >
+        N
+      </h1>
+      <h1
+        className=" info"
+        onClick={() => {
+          navigate("/contactme");
+        }}
+      >
+        T
+      </h1>
+      <h1
+        className=" info"
+        onClick={() => {
+          navigate("/contactme");
+        }}
+      >
+        A
+      </h1>
+      <h1
+        className=" info"
+        onClick={() => {
+          navigate("/contactme");
+        }}
+      >
+        C
+      </h1>
+      <h1
+        className=" info"
+        onClick={() => {
+          navigate("/contactme");
+        }}
+      >
+        T
+      </h1>
     </>
   );
 }
